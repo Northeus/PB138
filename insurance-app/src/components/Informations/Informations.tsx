665b1582414aac8e810c1604890b7308c4c81864
Jakub Maloštík <xmalost@fi.muni.cz>
2021-07-19 22:27:54 +0200

    changed from using <a> to <Link>

--- insurance-app/src/components/Informations/Informations.tsx
+++ insurance-app/src/components/Informations/Informations.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import BEMHelper from 'react-bem-helper';
+import { Link } from 'react-router-dom';
 import InfoPaths from '../../utils/infoPaths';
 import RoutingPaths from '../../utils/routingPaths';
 import './Informations.css';
@@ -12,24 +13,24 @@ interface InformationsProps {
 
 const Informations: React.FC<InformationsProps> = ({newTab}) => (
     <nav {...classes()}>
-        <a
+        <Link
             {...classes('item')}
             target={newTab ? '_blank' : undefined}
-            href={`${RoutingPaths.Info}/${InfoPaths.Contacts}`}>
+            to={`${RoutingPaths.Info}/${InfoPaths.Contacts}`}>
             <span {...classes('name')}>Kontaktujete nás</span>
-        </a>
-        <a
+        </Link>
+        <Link
             {...classes('item')}
             target={newTab ? '_blank' : undefined}
-            href={`${RoutingPaths.Info}/${InfoPaths.AboutUs}`}>
+            to={`${RoutingPaths.Info}/${InfoPaths.AboutUs}`}>
             <span {...classes('name')}>O nás</span>
-        </a>
-        <a
+        </Link>
+        <Link
             {...classes('item')}
             target={newTab ? '_blank' : undefined}
-            href={`${RoutingPaths.Info}/${InfoPaths.GDPR}`}>
+            to={`${RoutingPaths.Info}/${InfoPaths.GDPR}`}>
             <span {...classes('name')}>GDPR</span>
-        </a>
+        </Link>
     </nav>
 );
 
